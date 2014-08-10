8bcf60bf9de65ad8e5c824b31ad2d37ed3110e84
Tim Woods <timjameswoods@gmail.com>
2014-08-10 19:25:52 +0100

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,7 @@ class UsersController < ApplicationController
   def index
 	@users=User.paginate(page: params[:page], :per_page=>10)
   end
-  
-  
+    
   def new
     @user=User.new
   end
@@ -15,10 +14,11 @@ class UsersController < ApplicationController
   def show
 	puts "-----------------user controller show--------------"
     @user=User.find(params[:id])
+	@microposts=@user.microposts.paginate(page: params[:page])
   end
   
   def create
-    @user=User.new(user_params())
+    @user=User.new(user_params)
 	if @user.save
 		sign_in @user
 		flash[:success]="Welcome to the Sample App!"
@@ -30,17 +30,15 @@ class UsersController < ApplicationController
   
   def edit
 	puts "----------user controller edit-----------"
-	@user=User.find(params[:id])
   end
   
-  def update
-	#@user=User.find(params[:id])
-	if @user.update_attributes(user_params)
-		flash[:success]="Profile updated"
-		redirect_to @user
-	else
-		render 'edit'
-	end
+def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def destroy
@@ -56,17 +54,8 @@ class UsersController < ApplicationController
 		#admin is not a param, for example, to stop people forging an admin right
 	  params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
-	
- #before filters - run before doing anything else in the controller
- #top of the file has exceptions on when this should be checked
-	def signed_in_user
-		puts "----------------signed_in_user-------------"
-		unless signed_in?
-			puts "--------------user not signed in??????-------------"
-			store_location()
-			#redirect_to signin_url, notice:"Please sign in."
-		end
-	end
+
+	# before filters
 	
 	def correct_user
 		@user=User.find(params[:id])
@@ -74,7 +63,7 @@ class UsersController < ApplicationController
 	end
  
 	def admin_user
-#		redirect_to(home_url) unless current_user.admin?
+		redirect_to(home_url) unless current_user.admin?
 	end
  
     
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,56 +1,49 @@
 module SessionsHelper
 
-	def sign_in(user)
-		remember_token=User.new_remember_token
-		cookies.permanent[:remember_token]=remember_token
-		user.update_attribute(:remember_token, User.digest(remember_token))
-		self.current_user=user
-	end
-	
-	def sign_out
-		current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
-		cookies.delete(:remember_token)
-		self.current_user=nil
-	end
-	
-	
-	def signed_in?
-		puts "-------------signed_in? method--------------"
-		puts 		!@current_user.nil?
-		puts		@current_user.nil?
-		puts		@current_user
-		!@current_user.nil?
-	end
-	
-	def current_user
-		puts "--------------(1)current_user----------------"
-		remember_token=User.digest(cookies[:remember_token])
-		puts "remember_token="+remember_token
-		@current_user ||= User.find_by(remember_token: remember_token)
-	end
-	
-	def current_user?(user)
-		puts "----------(2)current_user?(user)------------"
-		user==current_user
-	end
-	
-	def current_user=(user)
-		puts "--------------(3)current_user=(user)----------------"
-		@current_user=user
-	end
-		
-	def redirect_back_or(default)
-		puts "-------------redirect_back_or(detault)-----------------"
-		#puts "session[:return_to] = "+session[:return_to]
-		redirect_to(session[:return_to] || default)
-		session.delete(:return_to)
-	end
-	
-	def store_location
-		puts "--------------store_location----------------"
-		session[:return_to]=request.url if request.get?
-		puts "store_location="+session[:return_to]
-	end
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+    self.current_user = user
+  end
 
+  def signed_in?
+    !current_user.nil?
+  end
 
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token  = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
 end
